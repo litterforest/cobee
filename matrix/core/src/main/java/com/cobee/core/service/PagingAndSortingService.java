4f0b1fe53238b9f2e386b7434fa1295fd7bc24fe
litterforest <18027041863@163.com>
2017-04-05 18:14:22 +0800

    完善泛型

--- matrix/core/src/main/java/com/cobee/core/service/PagingAndSortingService.java
+++ matrix/core/src/main/java/com/cobee/core/service/PagingAndSortingService.java
@@ -21,70 +21,65 @@ import org.mybatis.spring.SqlSessionUtils;
 import com.cobee.core.dao.CrudDao;
 import com.cobee.core.entity.BaseEntity;
 
-public abstract class PagingAndSortingService<T extends CrudDao<? extends BaseEntity<? extends Serializable>, ? extends Serializable>> extends BaseService<T> {
-	
-	private String getMapperNamespace()
-	{
+public abstract class PagingAndSortingService<T extends CrudDao<? extends BaseEntity<? extends Serializable>, ? extends Serializable>>
+		extends BaseService<T> {
+
+	private String getMapperNamespace() {
 		Type type = this.getClass().getGenericSuperclass();
-	    ParameterizedType parameterizedType = (ParameterizedType) type;
+		ParameterizedType parameterizedType = (ParameterizedType) type;
 		return parameterizedType.getActualTypeArguments()[0].getTypeName();
 	}
 
-	public <E> List<E> findByPage(E entity) {
-			String selectID = getMapperNamespace() + ".findList";
-			SqlSession session = SqlSessionUtils.getSqlSession(sqlSessionFactory);
-			
-			Configuration conf = session.getConfiguration();
-			
-			MappedStatement ms = conf.getMappedStatement(selectID);
-			BoundSql bs = ms.getBoundSql(entity);
-			String sql = bs.getSql();
-			sql = sql.toLowerCase();
-			System.out.println(sql);
-			Integer orderByIdx = sql.lastIndexOf("order by");
-			if (orderByIdx >= 0)
-			{
-				sql = sql.substring(0, orderByIdx);
+	public <E extends BaseEntity<? extends Serializable>> List<E> findByPage(E paramObj) {
+		String selectID = getMapperNamespace() + ".findList";
+		SqlSession session = SqlSessionUtils.getSqlSession(sqlSessionFactory);
+		Configuration conf = session.getConfiguration();
+		MappedStatement ms = conf.getMappedStatement(selectID);
+		BoundSql bs = ms.getBoundSql(paramObj);
+		String sql = bs.getSql();
+		sql = sql.toLowerCase();
+		System.out.println(sql);
+		Integer orderByIdx = sql.lastIndexOf("order by");
+		if (orderByIdx >= 0) {
+			sql = sql.substring(0, orderByIdx);
+		}
+		Integer limitIdx = sql.lastIndexOf("limit");
+		if (limitIdx >= 0) {
+			sql = sql.substring(0, limitIdx);
+		}
+		System.out.println(sql);
+		Integer fromIdx = sql.indexOf("from");
+		String countSql = "select count(1) " + sql.substring(fromIdx);
+		System.out.println(countSql);
+		Integer totalCount;
+		try {
+			Connection conn = session.getConnection();
+			PreparedStatement ps = conn.prepareStatement(countSql);
+			List<ParameterMapping> parameterMappingList = bs.getParameterMappings();
+			for (int i = 0; i < parameterMappingList.size(); i++) {
+				ParameterMapping pm = parameterMappingList.get(0);
+				ps.setObject(i + 1, PropertyUtils.getProperty(paramObj, pm.getProperty()));
 			}
-			Integer limitIdx = sql.lastIndexOf("limit");
-			if (limitIdx >= 0)
-			{
-				sql = sql.substring(0, limitIdx);
-			}
-			System.out.println(sql);
-			Integer fromIdx = sql.indexOf("from");
-			String countSql = "select count(1) " + sql.substring(fromIdx);
-			System.out.println(countSql);
-			Integer totalCount;
-			try {
-				Connection conn = session.getConnection();
-				PreparedStatement ps = conn.prepareStatement(countSql);
-				List<ParameterMapping> parameterMappingList = bs.getParameterMappings();
-				for (int i = 0; i < parameterMappingList.size(); i++)
-				{
-					ParameterMapping pm = parameterMappingList.get(0);
-					ps.setObject(i + 1, PropertyUtils.getProperty(entity, pm.getProperty()));
-				}
-				
-				ResultSet resultSet = ps.executeQuery();
-				resultSet.next();
-				totalCount = resultSet.getInt(1);
-				System.out.println(">>>>>>>>>>" + totalCount);
-			} catch (IllegalAccessException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InvocationTargetException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (NoSuchMethodException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			List<E> list = session.selectList(selectID, entity);
-			return list;
-		} 
-	
+
+			ResultSet resultSet = ps.executeQuery();
+			resultSet.next();
+			totalCount = resultSet.getInt(1);
+			System.out.println(">>>>>>>>>>" + totalCount);
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<E> list = session.selectList(selectID, paramObj);
+		return list;
+	}
+
 }
